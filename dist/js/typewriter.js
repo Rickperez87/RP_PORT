b6afb79d1a83f9164ef82be6aa86a22e109bede2
Rick Perez <Rickperez87@gmail.com>
2021-02-23 13:27:23 -0500

    Add fade in & move in lfet css animations to home

--- dist/js/typewriter.js
+++ dist/js/typewriter.js
@@ -40,7 +40,7 @@ class TypeWriter {
   }
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", setTimeout(init, 1500));
 
 function init() {
   const element = document.querySelector(".txt-type");
