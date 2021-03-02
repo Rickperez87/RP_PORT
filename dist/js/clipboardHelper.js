9467d3e543b82d68cca91936663e4b1f13975840
Rick Perez <Rickperez87@gmail.com>
2021-03-02 17:11:41 -0500

    copytoClipboard helper function

--- dist/js/clipboardHelper.js
+++ dist/js/clipboardHelper.js
@@ -1,6 +1,15 @@
-function clipToBoard() {
-  let copyText = document.querySelector(".copy-to-clipboard");
-  copyText.getAttribute("data-clipboard-text");
-  console.log("copied", copyText);
-  console.log("click");
+function clipToBoard(copyText) {
+  let textField = document.createElement("input");
+  textField.setAttribute("id", "textfield-copy-text");
+  textField.setAttribute("value", copyText);
+  let copyElement = document.querySelector(".copy-to-clipboard");
+  copyElement.appendChild(textField);
+
+  let textFieldCopyText = document.getElementById("textfield-copy-text");
+  textFieldCopyText.select();
+  textFieldCopyText.setSelectionRange(0, 99999); /* for mobile devices*/
+
+  document.execCommand("copy");
+
+  copyElement.removeChild(textField);
 }
