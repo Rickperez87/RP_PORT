5e419dca0d5715b1dbd1aba6f69307daf508f543
Rick Perez <Rickperez87@gmail.com>
2021-03-03 16:15:09 -0500

    clipboardHelper accepts querySelector argument

--- dist/js/clipboardHelper.js
+++ dist/js/clipboardHelper.js
@@ -1,8 +1,8 @@
-function clipToBoard(copyText) {
+function clipToBoard(copyText, querySelector) {
   let textField = document.createElement("input");
   textField.setAttribute("id", "textfield-copy-text");
   textField.setAttribute("value", copyText);
-  let copyElement = document.querySelector(".copy-to-clipboard");
+  let copyElement = document.querySelector(querySelector);
   copyElement.appendChild(textField);
 
   let textFieldCopyText = document.getElementById("textfield-copy-text");
