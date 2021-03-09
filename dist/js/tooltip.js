6d26b747c72decd462eab3f5e979e7eba54b551e
Rick Perez <Rickperez87@gmail.com>
2021-03-09 14:26:32 -0500

    fix tooltip target only active on btn click

--- dist/js/tooltip.js
+++ dist/js/tooltip.js
@@ -2,7 +2,7 @@ window.onload = tooltip("Copied to Clipboard!", ".tooltip-target");
 
 function tooltip(text, querySelectorParent) {
   let parentElement = document.querySelector(querySelectorParent);
-  document.addEventListener("click", () => {
+  parentElement.addEventListener("click", () => {
     let tooltip = document.createElement("div");
     tooltip.setAttribute("class", "tooltip");
     tooltip.innerHTML = text;
