27922893868b3916b882cf43010cf32ab5916b0b
Rick Perez <Rickperez87@gmail.com>
2021-04-08 16:16:19 -0400

    sectionAppear script appear sections with observer

--- dist/js/sectionAppear.js
+++ dist/js/sectionAppear.js
@@ -1,1 +1,25 @@
-console.log("testing");
+const allSections = document.querySelectorAll("section");
+
+const revealSections = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    return;
+  }
+  entry.target.classList.remove("section--hidden");
+  sectionObserver.unobserve(entry.target);
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.2,
+};
+
+const sectionObserver = new IntersectionObserver(revealSections, obsOptions);
+
+allSections.forEach((section) => {
+  if (!section.id) {
+    return;
+  }
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
